4d90780881001e617df18fa5c4bddeb014620447
Urte <urte.rimsaite@gmail.com>
2018-11-23 17:08:35 +0200

    sprint2 5-a uzduotis ir jos testas

--- src/test/java/TestKodas/TestIntersectionsOfRoutes.java
+++ src/test/java/TestKodas/TestIntersectionsOfRoutes.java
@@ -1,12 +1,30 @@
 package TestKodas;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kodas.IntersectionsOfRoutes;
+import lt.baltictalents.stoteliutinklas.data.beans.Station;
+import lt.baltictalents.stoteliutinklas.data.hardcode.HardCodedDb;
 
 public class TestIntersectionsOfRoutes {
 	
 	@Test
-	public void testIntersectionsOfRoutes() {
+	public void testfindIntersectionsOfRoutes() {
 		
+		IntersectionsOfRoutes target = new IntersectionsOfRoutes();
+		HardCodedDb db = new HardCodedDb();
+		
+		List<Station> testList1 = new ArrayList<Station>();
+		
+		assertEquals(testList1, target.findIntersectionsOfRoutes(new String[] {""}));
+		assertEquals(22, target.findIntersectionsOfRoutes(new String[] {"Autobusas 2G: Stotis - Santariškės"}).size());
+		assertEquals(7, target.findIntersectionsOfRoutes(new String[] {"Autobusas 48 Santariškės - Laisvės pr. - Šeškinė", 
+				"Autobusas 2G: Stotis - Santariškės"}).size());
+				
 	}
 
 }
